c0912088aae1ee38859e76a36db031f9608d6581
Piaczio <trotroyu@gmail.com>
2022-10-11 12:05:03 -0600

    fix

--- src/lib/vite.config.js
+++ src/lib/vite.config.js
@@ -1,10 +1,13 @@
+import preprocess from 'svelte-preprocess';
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 // add this import at the top:
-import preprocess from 'svelte-preprocess';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [svelte()]
+  plugins: [svelte({
+    preprocess: preprocess()
+  })]
 })
