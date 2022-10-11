4d6f1b6d179a8ba63312d784075ec9233bf37297
Lumexio <trotroyu@gmail.com>
2022-10-11 01:25:29 -0600

    fix: Card contact 1 of 3 update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+// add this import at the top:
+import preprocess from 'svelte-preprocess';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/portfolio/",
-  plugins: [svelte()]
+  plugins: [svelte({
+    preprocess: preprocess()
+  })]
 })
