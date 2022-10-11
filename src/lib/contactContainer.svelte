53bb00270e658eb177f4856708145abde206be9c
Piaczio <trotroyu@gmail.com>
2022-10-11 16:44:49 -0600

    feat: Send email to contact me

--- src/lib/contactContainer.svelte
+++ src/lib/contactContainer.svelte
@@ -15,16 +15,18 @@
     background-color: #212121;
     box-shadow: -0.5rem 0.5rem 0 #fab700;
     border: 3px solid #fab700;
+    
   }
   .temp {
     display: grid;
-    place-items: center;
+    place-items: start;
     grid-template-columns: repeat(1, minmax(5rem, 1fr));
   }
   input {
     outline: #e4e4e4;
     font-size: 1.1rem;
     margin: 0.5rem;
+    width: 100%;
   }
   input:hover {
     outline: #fab700;
@@ -33,7 +35,7 @@
     border-radius: 2px;
   }
   textarea {
-    min-width: 280px;
+    width: 100%;
     min-height: 100px;
     outline: #e4e4e4;
     font-size: 1.1rem;
@@ -45,13 +47,27 @@
     outline-style: double;
     border-radius: 2px;
   }
+  
+  #button-primary {
+    border-radius: 0.8em;
+    background-color: #fab700;
+    color: black;
+    margin: 0.5rem;
+    
+  }
+  #button-primary:active {
+    background-color: #e7fa00;
+  }
+  
 </style>
 
 <div id="Contact" class="home-container-style ">
   <div class="temp card">
-    <h1 id="elementEl">How to reach me</h1>
-    <input type="email" placeholder="Email" />
-    <input type="text" placeholder="Subject" />
-    <textarea placeholder="Messege" />
+    <h1>Talk to me</h1>
+    <form style="text-align: right;"    action="mailto:lumexio.dev@gmail.com" method="post" enctype="text/plain">
+      <input type="text" placeholder="Subject" />
+      <textarea type="text" placeholder="Messege" />
+      <button  type="submit" id="button-primary">Send</button>
+    </form>
   </div>
 </div>
