d8fa8f3aafaf87a612072333ef2aa5997e9ca75e
Lumexio <trotroyu@gmail.com>
2022-10-11 01:54:29 -0600

    fix: Input style

--- src/lib/contactContainer.svelte
+++ src/lib/contactContainer.svelte
@@ -13,7 +13,7 @@
   }
   .card {
     background-color: #212121;
-    box-shadow: 0.5rem 0.5rem 0 #fab700;
+    box-shadow: -0.5rem 0.5rem 0 #fab700;
     border: 3px solid #fab700;
   }
   .temp {
@@ -24,8 +24,22 @@
   input {
     outline: #e4e4e4;
     font-size: 1.1rem;
+    margin: 0.5rem;
   }
   input:hover {
+    outline: #fab700;
+    outline-width: 4px;
+    outline-style: double;
+    border-radius: 2px;
+  }
+  textarea {
+    min-width: 280px;
+    min-height: 100px;
+    outline: #e4e4e4;
+    font-size: 1.1rem;
+    margin: 0.5rem;
+  }
+  textarea:hover {
     outline: #fab700;
     outline-width: 4px;
     outline-style: double;
@@ -36,6 +50,8 @@
 <div id="Contact" class="home-container-style ">
   <div class="temp card">
     <h1 id="elementEl">How to reach me</h1>
+    <input type="email" placeholder="Email" />
     <input type="text" placeholder="Subject" />
+    <textarea placeholder="Messege" />
   </div>
 </div>
